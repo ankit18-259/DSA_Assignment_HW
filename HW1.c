964fcdf8dd7ed79ae007fa6579a3e8e0ac4a3672
ankit18-259 <ankit18_259@aec.ac.in>
2020-09-10 03:54:45 +0530

    3

--- HW1.c
+++ HW1.c
@@ -3,6 +3,7 @@ Include the following operations: Add at beginning, search , Display, exit*/
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
 
 struct Node {
    int data;
@@ -20,34 +21,23 @@ void push(struct Node **head_ref, int new_data)
 void print_ll(struct Node *head)
 {
    
-   while(head->next != NULL)
+   while(head != NULL)
    {
       printf("%d", head->data);
       head = head->next;
-      if(head->next == NULL)
-      {
-         printf("%d", head->data);
-      }
    }
 }
 
-void find_in_ll(struct Node *head,int s_data)
-{
-   int n = 1;
-   while(head->data != s_data)
-   {
-      if(head->data == s_data)
-      {
-         printf("Exists at node %d", n);
-      }
-            if(head->data != s_data)
-      {
-         printf("Doesn't Exist");
-      }
-      head = head->next;
-      n++;
-   }
-}
+bool search(struct Node* head, int x) 
+{  
+    while (head != NULL) 
+    { 
+        if (head->data == x) 
+            return true; 
+       head = head->next; 
+    } 
+    return false; 
+} 
 
 int main()
 {
@@ -68,12 +58,14 @@ int main()
          
          case 2:
             scanf("%d", &s_data);
-            find_in_ll(head, s_data);
+            search(head, s_data)? printf("Yes") : printf("No");
             break;
 
          case 3:
             print_ll(head);
          
+            break;
+         case 4:
             break;
       }
    }
